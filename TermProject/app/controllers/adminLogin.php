ffbe51d0e6b51485d8b2c4da231f7e0bf6840653
Raigorodskyi <70526919+Raigorodskyi@users.noreply.github.com>
2022-03-24 13:18:18 -0400

    Admin login controller

--- TermProject/app/controllers/adminLogin.php
+++ TermProject/app/controllers/adminLogin.php
@@ -9,11 +9,41 @@ class adminLogin extends Controller
     //TODO
     public function index()
     {
-        
+        if(!isset($_POST['login'])){
+            $this->view('adminLogin/index');
+        }
+        else{
+            $user = $this->adminLoginModel->getAdmin($_POST['AdminEmail']);
+            
+            if($user != null){
+                $hashed_pass = $user->AdminPassword;
+                $password = $_POST['AdminPassword'];
+                if(password_verify($password,$hashed_pass)){
+                    //echo '<meta http-equiv="Refresh" content="2; url=/MVC/">';
+                    $this->createSession($user);
+                    $data = [
+                        'msg' => "Welcome, $user->AdminFirstName $user->AdminLastName!",
+                    ];
+                    $this->view('Home/home',$data);
+                }
+                else{
+                    $data = [
+                        'msg' => "Password incorrect!",
+                    ];
+                    $this->view('adminLogin/index',$data);
+                }
+            }
+            else{
+                $data = [
+                    'msg' => "The admin password is incorrect!",
+                ];
+                $this->view('adminLogin/index',$data);
+            }
+        }
     }
 
     public function createSession($user){
-        
+            $_SESSION['AdminEmail'] = $user->AdminEmail;
     }
 
     public function logout(){
