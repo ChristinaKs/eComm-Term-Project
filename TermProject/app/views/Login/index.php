7b69c00278cf96cd5a7913323e11cf4f32d3b56f
Raigorodskyi <70526919+Raigorodskyi@users.noreply.github.com>
2022-03-24 13:14:40 -0400

    customer login view

--- TermProject/app/views/Login/index.php
+++ TermProject/app/views/Login/index.php
@@ -5,18 +5,25 @@
 <form class="px-4 py-3" method="post" action="">
     <div class="form-group">
         <label for="ClientEmail">Email</label>
-        <input type="text" class="form-control" id="ClientEmail" name="ClientEmail" placeholder="Please enter your email">
+        <input type="text" class="form-control" id="ClientEmail" name="ClientEmail" placeholder="ClientEmail">
     </div>
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password" placeholder="Please enter your password">
+        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
     </div>
     <div> 
         <button type="submit" name="login" class="btn btn-primary">Sign in</button>
         <p class="text-center">Not registered yet? <a href="/TermProject/Login/Create">Sign Up</a> </p>
-        <p class="text-center">Are you an admin?  <a href="/TermProject/adminLogin/index">Admin Login</a> </p>
+        <p class="text-center">You are an Admin? <a href="/TermProject/adminLogin/index">Admin Login</a> </p>
     </div>
     <?php
+
+    if($data != []){
+        echo '<div class="alert alert-danger" role="alert">'.
+            $data['msg'].'
+        </div>';
+    }
+
 ?>
 
 </form>
