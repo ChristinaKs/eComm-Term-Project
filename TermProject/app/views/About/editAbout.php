49217b71cfc9a1ea0f5a4d49a45ec3acc82e6fb2
ChristinaKs <74264367+ChristinaKs@users.noreply.github.com>
2022-04-07 10:36:15 -0400

    fixed about

--- TermProject/app/views/About/editAbout.php
+++ TermProject/app/views/About/editAbout.php
@@ -14,24 +14,25 @@
   </div>
 </nav>
 
+<?php
+$about = $data[0];
+?> 
     <h1>Update Products View</h1>
     
     <form action='' method='post' enctype="multipart/form-data">
 
     <div class="form-group">
-        <label for="firstparagraph">First Paragraph</label>
-        <input name="firstparagraph" type="text" class="form-control" id="firstparagraph" value="<?php echo $data->firstparagraph?>">
+        <label for="firstparagraph">First Paragraph: </label>
+        <input name="firstparagraph" type="text" class="form-control" id="firstparagraph" value="<?php echo $about->firstparagraph?>">
     </div><div class="form-group">
-        <label for="secondparagraph">Second Paragraph</label>
-        <input name="secondparagraph" type="text" class="form-control" id="secondparagraph" value="<?php echo $data->secondparagraph?>">
+        <label for="secondparagraph">Second Paragraph: </label>
+        <input name="secondparagraph" type="text" class="form-control" id="secondparagraph" value="<?php echo $about->secondparagraph?>">
     </div><div class="form-group">
-        <label for="thirdparagraph">ThirdParagraph</label>
-        <input name="thirdparagraph" type="text" class="form-control" id="thirdparagraph" value="<?php echo $data->thirdparagraph?>">
+        <label for="thirdparagraph">Third Paragraph: </label>
+        <input name="thirdparagraph" type="text" class="form-control" id="thirdparagraph" value="<?php echo $about->thirdparagraph?>">
     
-
+    <br>
     <button type="submit" name='update' class="btn btn-primary">Update</button>
-    <button type="submit" name='cancel' class="btn btn-primary">Cancel</button>
-    </form>
-
-   
+    <button id='cancel' name='cancel' class='btn btn-primary'> <a href='/TermProject/About/displayABout' >Cancel </a></button>
+    </form>   
 <?php require APPROOT . '/views/includes/footer.php'; ?>
